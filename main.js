056de69d9a327a9c680fc540bf93df0cd6b92395
Mégane <megane.londono@hotmail.com>
2021-10-13 16:34:05 +0200

    final changes, important button still not working

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const list = document.querySelector("ul");
 const form = document.querySelector("#add");
+const important = document.getElementById("important");
 const editBtn = document.querySelector(".edit");
 const deleteBtn = document.querySelector(".delete");
 const search = document.querySelector(".search input");
@@ -15,11 +16,13 @@ const addMovie = (movie, id) => {
             <div>Summary: ${movie.summary}</div>
             <div>Created on: ${time}</div>
             <br></br>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="important">
-                    <label class="form-check-label" for="important">IMPORTANT: To watch
-                </label>
-            </div>
+            <input id="important" type="checkbox" class="btn-check" id="check_${id}" active="${movie.important}" data-id="${id}" data-click="important">
+                <label class="btn btn-sm btn-outline-warning" for="check_${id}">
+                Important
+                <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" height="14px" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                </svg>
+            </label>
             <div class="btn btn-danger btn-sm my-2 edit">edit</div>
             <button class="btn btn-danger btn-sm my-2 delete">delete</button>
         </li>
@@ -27,14 +30,10 @@ const addMovie = (movie, id) => {
 
     list.innerHTML += html;
 
-    const checkbox = document.querySelector("#important");
-    
-    checkbox.addEventListener("click", e => {
-        if (e.target.tagName === "INPUT") {
-            e.target.parentElement.getAttribute("data-id");
-            console.log("hi there")
-        }
-    }); 
+    if (movie.important === true) {
+        let check_id = "check_" + id;
+        document.getElementById(check_id).checked = true;
+    }
 }
 
 const deleteMovie = (id) => {
@@ -124,3 +123,19 @@ search.addEventListener("keyup", () => {
     const term = search.value.trim().toLowerCase();
     filterMovies(term);
 }) 
+
+// "important" selection
+/* important.addEventListener("change", () => {
+    const movies = document.querySelectorAll("li");
+    if(important.checked === true){
+        movies.forEach(note => {
+        if(!movie.childNodes[1].childNodes[1].childNodes[3].checked){
+          movie.classList.add("d-none");
+        }
+      })
+    } else{
+        movies.forEach(movie => {
+        movie.classList.remove("d-none");
+      })
+    }
+}) */
